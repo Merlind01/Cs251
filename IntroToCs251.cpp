0369e99c14b4c700de763d9963733d7bd1eb8eb6
Merlind01 <166763380+Merlind01@users.noreply.github.com>
2024-08-26 11:47:24 -0400

    Add files via upload

---
+++ IntroToCs251.cpp
@@ -0,0 +1,21 @@
+/*
+Input: a Integer number
+Output: True/Fasle if its an even number
+
+function OddOrEven(int num) {
+    if (num% 2)=0;
+    return true;
+    else
+    return flase;
+}
+
+Common Data Structures
+    Arrays
+    Linked list
+    Stacks
+    Queues
+    Trees
+    Graphs
+
+Leetcode (Webstite for practice)
+*/
