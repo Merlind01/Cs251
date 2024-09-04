4d7f8c552ad8870ad81076ee84d2f6167c719e5e
Merlind01 <166763380+Merlind01@users.noreply.github.com>
2024-09-04 11:49:16 -0400

    LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -55,3 +55,107 @@ What operations can you perform on a single linked list
     - Applications
     - Deletion
 */
+
+
+
+#include <iostream>
+using namespace std;
+
+
+struct LinkedList{
+    int data;
+    LinkedList* next;
+    LinkedList(){}
+    LinkedList(int val)
+    {
+        this->data=val;
+        this->next=NULL;
+    }
+};
+
+
+LinkedList* insertIntoHead(LinkedList* head, int data){
+    LinkedList* newNode=new LinkedList(data);
+    newNode->next=head;
+    head=newNode;
+    return head;
+}
+
+
+LinkedList* insertIntoPosition(LinkedList* head, int data,int position){
+    LinkedList* newNode=new LinkedList(data);
+    LinkedList* prev=NULL;
+    LinkedList* cur=head;
+    int currentPosition=1;
+
+    while(cur!=NULL && position!=currentPosition){
+        prev=cur;
+        cur=cur->next;
+        currentPosition++;
+    }
+    
+    newNode->next=cur;
+    if(prev!=NULL) prev->next=newNode;
+    else head=newNode;
+    return head;
+}
+
+int getLinkedListLength(LinkedList* head)
+{
+    int length=0;
+    while(head!=NULL){
+        length++;
+        head=head->next;
+    }
+    return length;
+}
+
+void FindPos(LinkedList* head, int Position)
+{
+    LinkedList* curNode = head;
+    int tempnum = 1;
+    while (curNode->next != NULL && tempnum != Position) {
+        curNode = curNode->next;
+        tempnum++;
+    }
+    cout<<curNode->data;
+}
+
+LinkedList* insertIntoEnd(LinkedList* head, int val){
+    LinkedList* newNode=new LinkedList(val);
+    LinkedList* curNode = head;
+    while (curNode->next != NULL) {     //Check for last node
+        curNode = curNode->next;
+    }
+    curNode->next =newNode;     //set NUll as new node
+
+    return head;
+}
+
+void displayAllNodes(LinkedList* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int Position;
+
+    LinkedList* head=new LinkedList(10);
+    head=insertIntoHead(head,5);
+    head=insertIntoHead(head,7);
+    head=insertIntoHead(head,11);
+    head=insertIntoHead(head,24);
+    displayAllNodes(head);
+    head=insertIntoEnd(head, 3);      //Insert at end 
+    displayAllNodes(head);             // Check if inserted at end
+    head=insertIntoPosition(head,3,1);
+    displayAllNodes(head);
+    cin>>Position;
+    FindPos(head,Position);
+    cout<<endl;
+    return 0;
+}
