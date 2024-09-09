685e8861dd627e27a130a639f6d2bbd5e5c8ec95
Merlind01 <166763380+Merlind01@users.noreply.github.com>
2024-09-09 12:32:18 -0400

    Sorting.cpp

--- Sorting.cpp
+++ Sorting.cpp
@@ -2,4 +2,31 @@
 Sorting: an operation that rearranges unordered items into ordered itmes according to a specified
 criteria
 
+A = [1,2,4,3]   Unsorted
+A = [1,2,3,4]   Sorted
+
+By default code should be sorted in ascending order
+
+Types of sorting algorithms (* Have to Know Well)
+*    -Bubble Sort           -Quick Sort
+*    -Selection Sort        -Counting Sort
+*    -Insertion Sort
+     -Merge Sort
+     -Heap Sort
+                     Time and Space Complexity varies 
+    Selection Sort = n^2
+    Insertion Sort = n^2
+    Merge Sort = n log(n)
+    Quick Sort = n log(n)
+    Counting Sort = n
+
+Bubble Sort 
+    - Bubble up the highest value (ascending order)
+    - Ascending order: low values to right values (left to right)
+    Case 1:
+    [4] [1]
+        Bubble up [4] to the place of 1 (by checking than moving up or going to the other pair)
+    [1] [4]
+
+
 */
