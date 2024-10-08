bc58585a6737178717b01b669864d984886c6776
Merlind01 <166763380+Merlind01@users.noreply.github.com>
2024-10-08 16:12:40 -0400

    PriorityQueue.cpp

---
+++ PriorityQueue.cpp
@@ -0,0 +1,22 @@
+/*    (L - Last / F - First / I - In / O - Out)
+  Stack         Queue        Priority Queue 
+  LIFO          FIFO          Based On Priority (Max or Min value has highest Priority)
+
+Priority Queue     (By Default Min value has highest priority)
+  - A type of queue that arranges elemetns based on their priority values. Elements with higher priority values are typically retrieved or removed before elements with lower 
+  priority values.
+
+Operation              Return Value                    Preview 
+Insert(C)                                              C
+Insert(O)                                              CO 
+Insert(D)                                              COD
+remove max                 O                           CD
+Insert(I)                                              CDI
+
+etc
+
+
+
+
+
+*/
