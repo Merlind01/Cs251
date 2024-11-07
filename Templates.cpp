dcb981c0530916743a5609c82d60c0bfc5e17474
Merlind01 <166763380+Merlind01@users.noreply.github.com>
2024-11-07 16:23:36 -0500

    Templates.cpp

---
+++ Templates.cpp
@@ -0,0 +1,36 @@
+/* 
+Polymorphism
+  Making something change to fit your needs
+
+Small function to find max of to int
+  int maximum (int a int b)
+  {
+  if (a > B)
+    return a 
+  else 
+    return b
+  }
+When Calling
+  maximum(30, 20)
+
+Templates are used when there is alot of data needed to go through a simple function
+  Template Function for Max:
+    template <class item>
+    Item maximum (item a, item b)
+    {
+      if (a>b)
+      return a
+      else return b
+    }
+
+Templates allow you to create a function such as 
+  print array and use that one function to print multiple arrays of different datatypes like: int,float,char etc
+  adding and use that one function to either add  numbers or connect strings of different datatypes like: char,float,double,int,string etc
+
+
+
+      
+
+
+
+*/
