de9013b8b78dc12b9d6175100fd4b73db3a2eff5
Merlind01 <166763380+Merlind01@users.noreply.github.com>
2024-11-20 11:21:04 -0500

    ReviewFinalCs251.cpp

---
+++ ReviewFinalCs251.cpp
@@ -0,0 +1,33 @@
+/* 
+Unique data structures / Important topics
+ - Optional (if answer is 0 will return no pointer) / For very specific cases
+ - Bloom Filter (bit array) (uses K-Hash functions)
+ - Rope (similar to string / intert to front,middle, or back)
+ - Skip Lists
+ - Zippers (Cursors / Current Location of the Cursor)
+ - Heaps (Min / Max)
+ - Hash Tables (For sorting info that gets ordered at a large amount)        **** 
+       Table saves location of data
+       
+       []  <- First peice of data might go here
+       []
+       []
+       []  <- second might go here (Doesnt require going from top to bottom)
+       []
+       []
+       
+       Collision may occur when there is not a space in between data for example to store 10 values its recommended to make a hash table that can store 20:
+              To clear a collision another hash table can be made
+
+       [A]
+       [ ]  <- A2 asked to be placed so will go here
+       [B]
+       [ ]    
+       [C]
+
+ - Fractals
+
+
+
+
+*/
